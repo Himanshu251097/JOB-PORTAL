34e229b6a5b2afb314e5c464c57d54d0b0ca5d6e
Himanshu251097 <2510singhhimanshu@gmail.com>
2025-06-10 21:20:53 +0530

    another value changed in variable

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 import JWT from "jsonwebtoken";
 
-const a = 15;
+const a = 20;
 const userAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer")) {
